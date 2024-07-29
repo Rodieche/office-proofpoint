f30727f7959e4f689eb1a1b02a67195b5c63cca2
Rudolf Echenique @ Expert I.T <rudolf@expertit.co.nz>
2024-07-29 14:27:03 +1200

    added Author on package.json, modified application.ps1 to check if nodejs was previosly installed

--- application.ps1
+++ application.ps1
@@ -1,3 +1,13 @@
+# Check if Node.js is installed
+if (-Not (Get-Command node -ErrorAction SilentlyContinue)) {
+    Write-Error "Node.js is not installed. Please install Node.js and try again."
+    exit 1
+}
+
+if (-Not (Test-Path -Path .\node_modules)) {
+    npm install
+}
+
 # Run the PowerShell script
 .\\src\\powershell\\v2Exchange.ps1
 
@@ -10,7 +20,6 @@ if ($LASTEXITCODE -eq 0) {
     if ($LASTEXITCODE -eq 0) {
         # Open the Excel file
         Invoke-Item .\\output\\Digest.xlsx
-        # Start-Process excel .\\output\\Digest.xlsx
     } else {
         Write-Error "The Node.js application failed to run."
     }
